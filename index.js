b7644445241c8393b92b81777fe0162f5f25ab35
HappyDev <67974781+D3R50N@users.noreply.github.com>
2022-07-25 11:17:21 +0100

    un commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-
+function func(value, array = this.array) {
+}
 class jat {
     constructor(array = []) {
         this.array = array;
@@ -14,14 +15,14 @@ function valObj(key, value, arr = []) {
 }
 
 
-jat.prototype.isEmpty = function isEmpty(array = this.array) {
+function isEmpty(array = this.array) {
     return array.length == 0;
 }
 
-jat.prototype.lastKey = function lastKey(array = this.array) {
+function lastKey(array = this.array) {
     return array.length - 1;
 }
-jat.prototype.find = function find(value, array = this.array) {
+function find(value, array = this.array) {
     var out = [];
     array.forEach((val, key) => {
         if (val != value)
@@ -33,19 +34,34 @@ jat.prototype.find = function find(value, array = this.array) {
     return out;
 }
 
-jat.prototype.findFirst = function findFirst(value, array = this.array) {
+function findFirst(value, array = this.array) {
     return this.find(value, array)[0] ? this.find(value, array)[0] : -1;
 }
-jat.prototype.findLast = function findLast(value, array = this.array) {
+function findLast(value, array = this.array) {
     return this.find(value, array)[this.lastKey(array)] ? this.find(value, array)[0] : -1;
 }
-jat.prototype.isIn = function isIn(value, array = this.array) {
+function isIn(value, array = this.array) {
     return !this.isEmpty(this.find(value, array));
 }
 
+function add(value, array = this.array) {
+
+}
 
 
-function newJat(array=[]){
+
+
+
+
+jat.prototype.isEmpty = isEmpty;
+jat.prototype.lastKey = lastKey;
+jat.prototype.find = find;
+jat.prototype.findFirst = findFirst;
+jat.prototype.findLast = findLast;
+jat.prototype.isIn = isIn;
+
+
+function newJat(array = []) {
     return new jat(array);
 }
 
